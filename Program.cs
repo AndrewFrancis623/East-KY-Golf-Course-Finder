aa559322f9e3f230bbde9e7e97c12594716553ba
Andrew Francis <afrancis623@icloud.com>
2023-03-27 06:57:12 -0400

    Added my project

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace FinalProject 
+{
+    // Main Class
+    public class Program 
+    {
+        // Start program
+        static void Main(string[] args) 
+        {
+            GetAppInfo(); //Run GetAppInfo function 
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+            // Get and display app info
+            static void GetAppInfo()
+            {
+                // Set app vars
+                string appName = "Final Project";
+                string appVersion = "1.0";
+                string appAuthor = "Andrew Francis";
+
+                // Change text color
+                Console.ForegroundColor = ConsoleColor.Cyan;
+
+                // Write out app info
+                Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+                // Reset Color
+                Console.ResetColor();
+
+            }
+          
+
+        }
+
+
+    }
+
+
+}
