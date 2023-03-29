fc98377054970c016944fbd95ad57c3cd7abce06
Andrew Francis <afrancis623@icloud.com>
2023-03-29 01:19:43 -0400

    added new class

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SkillLevel;
 
 namespace FinalProject 
 {
@@ -9,8 +10,58 @@ namespace FinalProject
         static void Main(string[] args) 
         {
             GetAppInfo(); //Run GetAppInfo function 
+          
+            while(true) 
+            {
+                //Get skill level
+                Console.WriteLine("***** East KY Golf Course Finder *****");
+                Console.WriteLine("Hello Golfer! \nIn order to find the right course for you we'll need a little info. \nPlease enter the number that corresponds to your skill level in the space below.");
+                Console.WriteLine("\t1 Begginer: Just started, I don't know what par is." + "\n\t2 Intermediate: I play frequently, I can get close to par on a good day." + "\n\t3 Professional: I play alot, I'm usally under par.");
+                
+                string skilllevel = Console.ReadLine(); //Get input from user
 
-            
+
+                if (skilllevel == "1")
+                {
+                    Console.WriteLine("correct");
+
+                }
+                else if (skilllevel == "Intermediate")
+                {
+                    Console.WriteLine("getting better");
+
+                }
+                else if (skilllevel == "Professional")
+                {
+                    Console.WriteLine("Hello Tiger Woods");
+
+                }
+                else 
+                {
+                    Console.ForegroundColor= ConsoleColor.Red;
+                    Console.WriteLine("Invalid Entry");
+                    Console.ResetColor();
+                
+                }
+
+                // Ask if ready to exit or restart
+                Console.WriteLine("Would you like to select another course? [Y or N]");
+
+                // Get answer
+                string answer = Console.ReadLine().ToUpper();
+
+                if (answer == "Y")
+                {
+                    continue;
+
+                }
+                else if (answer == "N")
+                {
+                    return;
+                }
+                else { }
+
+            }
 
 
 
